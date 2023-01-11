578c3399e8c7464d40fa36659e088c3c03cf0b2b
Tyluur <itstyluur@icloud.com>
2023-01-11 02:40:51 -0500

    feat: update kotlin build script to minimal dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("jvm") version "1.8.0"
 }
-
-val koinVersion = "2.1.5"
-val junitVersion = "5.6.2"
-val jacksonVersion = "2.12.2"
 
 allprojects {
     apply(plugin = "kotlin")
@@ -30,52 +24,9 @@ allprojects {
 }
 
 dependencies {
-    // Java
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
     implementation(kotlin("gradle-plugin", version = "1.5.0"))
-
-    // Kotlin
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.4.2")
-
-    // Dependency Injection
-    implementation("org.koin", "koin-core", koinVersion)
-    implementation("org.koin", "koin-logger-slf4j", koinVersion)
-
-    // Reflection
-    implementation("io.github.classgraph", "classgraph", "4.8.78")
-
-    // Logging
-    implementation("ch.qos.logback:logback-classic:1.2.3") {
-        exclude("org.slf4j", "slf4j-jdk14")
-    }
-    implementation("com.michael-bull.kotlin-inline-logger", "kotlin-inline-logger-jvm", "1.0.2")
-
-    // RuneScape API
-    implementation("com.displee", "rs-cache-library", "6.7")
-    implementation("com.github.michaelbull", "rs-api", "1.1.1")
-
-    //Utilities
-    implementation("com.google.guava:guava:29.0-jre")
-    implementation("org.apache.commons:commons-lang3:3.10")
-    implementation("commons-cli", "commons-cli", "1.4")
-
-    implementation("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
-    implementation("com.fasterxml.jackson.module:jackson-module-paranamer:$jacksonVersion")
-    implementation("com.fasterxml.jackson.module:jackson-module-parameter-names:$jacksonVersion")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:$jacksonVersion")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
-
-    // Network
-    implementation("io.netty:netty-all:4.1.44.Final")
-
-    //Testing
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
-    testImplementation(group = "org.koin", name = "koin-test", version = koinVersion)
-    testImplementation(group = "io.mockk", name = "mockk", version = "1.10.0")
 }
 
 tasks {
